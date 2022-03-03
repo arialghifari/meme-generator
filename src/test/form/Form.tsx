9337e1495e6b5cc1dbb0e66e05745fefbe74e107
arialghifari <arialghifari99@gmail.com>
2022-03-04 00:29:43 +0800

    Test form state object

--- src/test/form/Form.tsx
+++ src/test/form/Form.tsx
@@ -1,38 +1,51 @@
 import React from "react";
 
 const Form = () => {
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
-  const handleChange = (event: any) => {
-    const inputValue = event.target.value;
-
-    if (event.target.name === "firstName") {
-      return setFirstName(inputValue);
-    }
-
-    return setLastName(inputValue);
+  const handleChange = (e: any) => {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [e.target.name]: e.target.value,
+      };
+    });
   };
 
   return (
     <div className="mx-auto w-fit mt-10">
-      <input
-        type="text"
-        name="firstName"
-        placeholder="First Name"
-        className="border border-black p-2"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="lastName"
-        placeholder="Last Name"
-        className="border border-black p-2"
-        onChange={handleChange}
-      />
-      <p className="mt-5 font-semibold">
-        {firstName} {lastName}
-      </p>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          className="border border-black p-2"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          className="border border-black p-2"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          className="border border-black p-2"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mt-5 font-semibold">
+        <p>First Name: {formData.firstName}</p>
+        <p>Last Name: {formData.lastName}</p>
+        <p>Email: {formData.email}</p>
+      </div>
     </div>
   );
 };

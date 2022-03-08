710132a09fa1313a43dcaa70a9618eed332bb2a9
arialghifari <arialghifari99@gmail.com>
2022-03-08 10:24:59 +0800

    Test re-render & sync useEffect from api with local state

--- src/api/TestApi.tsx
+++ src/api/TestApi.tsx
@@ -2,32 +2,24 @@ import React from "react";
 
 const TestApi = () => {
   const [starWarsData, setStarWarsData] = React.useState({});
-  let [count, setCount] = React.useState(0);
-
-  console.log("Component rendered");
+  let [count, setCount] = React.useState(1);
 
   React.useEffect(() => {
-    console.log("useEffect ran");
-
-    fetch("https://swapi.dev/api/people/1/")
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then((res) => res.json())
       .then((data) => setStarWarsData(data));
-  }, []);
-
-  const handleAdd = () => {
-    setCount((prevCount) => (prevCount += 1));
-  };
+  }, [count]);
 
   return (
     <div>
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       <h1>COUNT: {count}</h1>
       <button
-        className="bg-blue-500 px-4 rounded-md text-white"
-        onClick={handleAdd}
+        className="bg-blue-500 py-1 px-4 rounded-md text-white"
+        onClick={() => setCount((prevCount) => prevCount + 1)}
       >
-        +1
+        Get Next Character
       </button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 };
